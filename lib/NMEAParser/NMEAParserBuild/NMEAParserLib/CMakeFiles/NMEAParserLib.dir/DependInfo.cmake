958743f4be59ec5a560164a832d3ab0491e5db22
Nathan Phipps O'Neill <nathan.phippsoneill2@mail.dcu.ie>
2021-07-19 15:47:13 +0100

    Added NMEAParse to the commit history

---
+++ lib/NMEAParser/NMEAParserBuild/NMEAParserLib/CMakeFiles/NMEAParserLib.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/Software/NMEAParserLib/NMEAParser.cpp" "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/NMEAParserBuild/NMEAParserLib/CMakeFiles/NMEAParserLib.dir/NMEAParser.cpp.o"
+  "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/Software/NMEAParserLib/NMEAParserPacket.cpp" "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/NMEAParserBuild/NMEAParserLib/CMakeFiles/NMEAParserLib.dir/NMEAParserPacket.cpp.o"
+  "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/Software/NMEAParserLib/NMEASentenceBase.cpp" "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/NMEAParserBuild/NMEAParserLib/CMakeFiles/NMEAParserLib.dir/NMEASentenceBase.cpp.o"
+  "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/Software/NMEAParserLib/NMEASentenceGGA.cpp" "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/NMEAParserBuild/NMEAParserLib/CMakeFiles/NMEAParserLib.dir/NMEASentenceGGA.cpp.o"
+  "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/Software/NMEAParserLib/NMEASentenceGSA.cpp" "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/NMEAParserBuild/NMEAParserLib/CMakeFiles/NMEAParserLib.dir/NMEASentenceGSA.cpp.o"
+  "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/Software/NMEAParserLib/NMEASentenceGSV.cpp" "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/NMEAParserBuild/NMEAParserLib/CMakeFiles/NMEAParserLib.dir/NMEASentenceGSV.cpp.o"
+  "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/Software/NMEAParserLib/NMEASentenceRMC.cpp" "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/NMEAParserBuild/NMEAParserLib/CMakeFiles/NMEAParserLib.dir/NMEASentenceRMC.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lib/NMEAParser/NMEAParserBuild/NMEAParserTest/CMakeFiles/NMEAParserTest.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/Software/NMEAParserTest/main.cpp" "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/NMEAParserBuild/NMEAParserTest/CMakeFiles/NMEAParserTest.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/Software/NMEAParserTest/../NMEAParserLib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/masters_project/binauralsensoryaugmentation/lib/NMEAParser/NMEAParserBuild/NMEAParserLib/CMakeFiles/NMEAParserLib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
